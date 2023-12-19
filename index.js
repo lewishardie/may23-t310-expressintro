aa89700b920ff76f7521ad8fecde1c6de691c891
Lewis Hardie <lewishardie@gmail.com>
2023-12-19 20:03:33 +1100

    nodemon, and PORT

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const express = require('express');
 // make an instance of the server that we can customise and run
 const app = express();
 
+
+const PORT = process.env.PORT || 3000;
+//ports run in integers  between 1001 - 65536
+
 // write the routes
 //Get localhost:3000/
 // app.get(route path, callback fucntion to handle request and response)
 app.get('/', (request, response) => {
     response.send("hellow world, server building in progress...")
 })
+
 // Configure the server
 
 // Activate the server
-app.listen(() => {
-    console.log("Server is running on port 3000");
-})
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
